d4ea4eb183ecf6c72b69a1bb76e24c820d740c49
Shir Bar Lev <shiruchii@gmail.com>
2020-05-19 15:14:12 +0300

    Added the new components & services to the module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser'
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component'
-import { newItems } from './components/newItems/newItems.component'
-import { ItemsService } from './services/items.service'
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { newItems } from './components/newItems/newItems.component';
+import { suggestedItems } from './components/suggestedItems/suggestedItems.component';
+import { siteHeader } from './components/header/header.component';
+import { userPage } from './components/userPage/userPage.component';
+import { ItemsService } from './services/items.service';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [newItems],
-  providers: [ItemsService],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    newItems,
+    suggestedItems,
+    siteHeader,
+    userPage
+  ],
+  providers: [
+    ItemsService,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {

6ac736ac223d2225df5d7868ab2386d910af79bf
Shir Bar Lev <shiruchii@gmail.com>
2020-05-19 17:25:14 +0300

    Added the main page to the router list.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MainPage } from './components/mainPage/mainPage.component';
 import { userPage } from './components/userPage/userPage.component';
 import { AppMessaging } from './components/messages/messages.component';
 
 const routes: Routes = [
+  { path: '', component: MainPage },
   { path: '/user', component: userPage },
   { path: '/messages', component: AppMessaging }
 ];

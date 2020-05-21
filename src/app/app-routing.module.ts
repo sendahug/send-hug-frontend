b386cda436148bb3e3bc15f2aed32866ec12ba2e
Shir Bar Lev <shiruchii@gmail.com>
2020-05-21 13:22:47 +0300

    Added the new component to the module and router.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainPage } from './components/mainPage/mainPage.component';
-import { userPage } from './components/userPage/userPage.component';
+import { UserPage } from './components/userPage/userPage.component';
 import { AppMessaging } from './components/messages/messages.component';
 import { ErrorPage } from './components/errorPage/errorPage.component';
+import { NewItem } from './components/newItem/newItem.component';
 
 const routes: Routes = [
   { path: '', component: MainPage },
-  { path: '/user', component: userPage },
+  { path: '/user', component: UserPage },
   { path: '/messages', component: AppMessaging },
+  { path: '/new/:type', component: NewItem },
   { path: '**', component: ErrorPage }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module'
 import { MainPage } from './components/mainPage/mainPage.component';
-import { userPage } from './components/userPage/userPage.component';
+import { UserPage } from './components/userPage/userPage.component';
 import { AppMessaging } from './components/messages/messages.component';
 import { ErrorPage } from './components/errorPage/errorPage.component';
+import { NewItem } from './components/newItem/newItem.component';
 import { ItemsService } from './services/items.service';
 import { AuthService } from './services/auth.service';
 
@@ -17,9 +18,10 @@ import { AuthService } from './services/auth.service';
   ],
   declarations: [
     MainPage,
-    userPage,
+    UserPage,
     AppMessaging,
-    ErrorPage
+    ErrorPage,
+    NewItem
   ],
   providers: [
     ItemsService,

8ad91def71f2099584c8018747c9b5ef43a25b9c
Shir Bar Lev <shiruchii@gmail.com>
2020-06-09 11:55:57 +0300

    Added environment variables.
    -In models.py, this includes the user used to access
    the databse.
    -In Angular production environment, this includes
    the requried Auth0 variables.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  auth0: {
+    domain: 'dev-sbac.auth0.com',
+    clientID:'rgZL4I04pep3P2GRIEVQtDkWcHjv9sru',
+    audience: 'sendhug',
+    redirectUri: 'http://localhost:3000/user',
+    logoutUri: 'http://localhost:3000'
+  }
 };

8e6596b6dd5564b49c9d13e8b89675edadfde085
Shir Bar Lev <shiruchii@gmail.com>
2020-06-11 16:46:05 +0300

    Testing fix for environment variables issue.

--- env-config.js
+++ env-config.js
@@ -6,7 +6,7 @@ fs.readFile('dist/app.bundle.min.js', 'utf8', function (err,data) {
     return console.log(err);
   }
   var result = data.replace(/{production:e.env.production,auth0:{domain:e.env.auth0domain,clientID:e.env.auth0client,audience:e.env.audience,redirectUri:e.env.loginurl,logoutUri:e.env.logouturl},backend:{domain:e.env.apiurl}}/g,
-    `{production:${process.env.production},auth0:{${domain:process.env.auth0domain},clientID:${process.env.auth0client},audience:${process.env.audience},redirectUri:${process.env.loginurl},logoutUri:${process.env.logouturl}},backend:{${domain:process.env.apiurl}}}`);
+    `{production:${process.env.production},auth0:{domain:${process.env.auth0domain},clientID:${process.env.auth0client},audience:${process.env.audience},redirectUri:${process.env.loginurl},logoutUri:${process.env.logouturl}},backend:{domain:${process.env.apiurl}}}`);
 
   fs.writeFile('dist/app.bundle.min.js', result, 'utf8', function (err) {
      if (err) return console.log(err);

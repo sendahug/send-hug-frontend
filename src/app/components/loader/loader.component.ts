bf6ee977a3a3002c81a7c397c63013cb84c09774
Shir Bar Lev <shiruchii@gmail.com>
2020-06-14 16:37:56 +0300

    Fixed an error where loader remained onscreen after messages were loaded.

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -153,6 +153,27 @@ export class Loader implements OnInit, OnChanges {
         }
       })
     }
+    //
+    else if(this.waitingFor == 'thread messages') {
+      this.message = 'Fetching messages...';
+      // subscribe to the subject following user data
+      this.authService.isUserDataResolved.subscribe((value) => {
+        // if user data has been fetched, the app can attempt to fetch
+        // the logged in user's threads
+        if(value == true) {
+          // subscribe to the subject following user threads data
+          this.itemsService.isThreadResolved.subscribe((value) => {
+            // the subject's value is changed to 'true' upon fetching user
+            // threads, so if the value is true, there's no longer need
+            // for the loader screen
+            if(value == true) {
+              this.visible = false;
+              this.waitingFor = '';
+            }
+          })
+        }
+      })
+    }
     // if the app is waiting for the user's posts to be fetched from the server
     else if(this.waitingFor == 'user posts') {
       this.message = 'Fetching user posts...';

1e1c5234833081382fb68c5ff50aed7a544ec268
Shir Bar Lev <shiruchii@gmail.com>
2020-06-19 15:05:55 +0300

    Added admin dashboard (main page) text.

--- src/app/components/adminDashboard/adminDashboard.component.ts
+++ src/app/components/adminDashboard/adminDashboard.component.ts
@@ -16,6 +16,21 @@ import { AuthService } from '../../services/auth.service';
 })
 export class AdminDashboard {
   screen:string = '';
+  adminCategories = [
+    {
+      title: 'Reports',
+      explanation: `Here you can view all the posts and users who've been reported by users. For post reports, you can either edit or delete the reported post. For user reports, you can edit or block the user.
+                    If you find no justified reason for the report (for either a user or a post), you can also dismiss the report without taking action on the post or the user.`
+    },
+    {
+      title: 'Blocks',
+      explanation: `Here you can view all blocked users and how long they've been block for. You can also block or unblock a user by their ID.`
+    },
+    {
+      title: 'Filters',
+      explanation: `Here you can view currently filtered words. You can also add or remote filtered words to the list.`
+    }
+  ]
 
   // CTOR
   constructor(

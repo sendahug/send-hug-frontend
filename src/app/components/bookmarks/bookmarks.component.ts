474b2c4d53f1a10920de1c32e0cf577e1e8e3ba2
Shir Bar Lev <shirblc@gmail.com>
2020-06-25 22:09:25 +0300

    Added method to fetch user bookmarks.

--- src/app/components/bookmarks/bookmarks.component.ts
+++ src/app/components/bookmarks/bookmarks.component.ts
@@ -4,23 +4,23 @@
 */
 
 // Angular imports
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 
 // App-related imports
-import { BookmarksService } from '../../services/bookmarks.service';
+import { BookmarkType, BookmarksService } from '../../services/bookmarks.service';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
 	selector: 'app-bookmarks',
 	templateUrl: './bookmarks.component.html'
 })
-export class Bookmarks {
+export class Bookmarks implements OnInit, OnChanges {
 	bookmarkTypes = [
 		{ name: 'Posts', param: 'posts' },
 		{ name: 'Messages', param: 'messages' },
 		{ name: 'Users', param: 'users' }
 	];
-	currentType = 'Posts';
+	currentType:BookmarkType = 'Posts';
 
 	// CTOR
 	constructor(
@@ -28,5 +28,29 @@ export class Bookmarks {
 		public authService:AuthService
 	) {
 
+	}
+
+	/*
+  Function Name: ngOnInit()
+  Function Description: This method is automatically triggered by Angular upon
+                        page initiation. Checks for the type of bookmarks viewed.
+  Parameters: None.
+  ----------------
+  Programmer: Shir Bar Lev.
+  */
+	ngOnInit() {
+		this.bookmarksService.getBookmarks(this.currentType);
+	}
+
+	/*
+  Function Name: ngOnChanges()
+  Function Description: This method is automatically triggered by Angular upon
+                        changes in parent component. Checks for the type of bookmarks viewed.
+  Parameters: None.
+  ----------------
+  Programmer: Shir Bar Lev.
+  */
+	ngOnChanges() {
+		this.bookmarksService.getBookmarks(this.currentType);
 	}
 }
--- src/app/services/bookmarks.service.ts
+++ src/app/services/bookmarks.service.ts
@@ -12,14 +12,19 @@ import { BehaviorSubject } from 'rxjs';
 import { Post } from '../interfaces/post.interface';
 import { Message } from '../interfaces/message.interface';
 import { OtherUser } from '../interfaces/otherUser.interface';
+import { AuthService } from './auth.service';
 import { AlertsService } from './alerts.service';
 import { environment } from '../../environments/environment';
 import { environment as prodEnv } from '../../environments/environment.prod';
+
+export type BookmarkType = 'Posts' | 'Messages' | 'Users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookmarksService {
+  readonly serverUrl = environment.production ? prodEnv.backend.domain! : environment.backend.domain;
+
   // bookmarks variables
   bookmarks: {
     Posts: Post[],
@@ -47,7 +52,57 @@ export class BookmarksService {
   }
 
   // CTOR
-  constructor() {
+  constructor(
+    private Http:HttpClient,
+    private authService:AuthService,
+    private alertsService:AlertsService
+  ) {
 
+  }
+
+  /*
+  Function Name: getBookmarks()
+  Function Description: Gets a paginated list of the given bookmarks type.
+  Parameters: type (string) - the type of bookmarks to fetch.
+  ----------------
+  Programmer: Shir Bar Lev.
+  */
+  getBookmarks(type:BookmarkType) {
+    const Url = this.serverUrl + `/bookmarks/${type}`;
+    const params = new HttpParams().set('page', `${this.bookmarksPage[type]}`);
+
+    // send the request
+    this.Http.get(Url, {
+      headers: this.authService.authHeader,
+      params: params
+    }).subscribe((response:any) => {
+      // if the type of fetched bookmarks is users
+      if(type == 'Users') {
+        let users = response.users;
+        users.forEach((element:any) => {
+          let user:OtherUser = {
+            id: element.id,
+            displayName: element.displayName,
+            givenHugs: element.givenH,
+            receivedHugs: element.receivedH,
+            postsNum: element.postsNum,
+            role: element.role
+          }
+          this.bookmarks[type].push(user);
+        });
+      }
+      // otherwise the array can be taken as is
+      else {
+        this.bookmarks[type] = response[type.toLowerCase()];
+      }
+      // set the page variables
+      this.bookmarksPage[type] = response.current_page;
+      this.totalBookmarksPages[type] = response.total_pages;
+      this.isBookmarksResolved[type].next(true);
+    }, (err:HttpErrorResponse) => {
+      // if there was an error, alert the user
+      this.isBookmarksResolved[type].next(true);
+      this.alertsService.createErrorAlert(err);
+    })
   }
 }

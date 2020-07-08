d7f265567187d65944ea3fa2fab4d108930ec78f
Shir Bar Lev <shirblc@gmail.com>
2020-07-08 14:26:02 +0300

    Added a button to toggle push notifications.

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -4,7 +4,7 @@
 */
 
 // Angular imports
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 
 // App-relateed imports
 import { AuthService } from '../../services/auth.service';
@@ -14,7 +14,10 @@ import { NotificationService } from '../../services/notifications.service';
   selector: 'app-notifications',
   templateUrl: './notifications.component.html'
 })
-export class NotificationsTab {
+export class NotificationsTab implements OnChanges {
+  pushStatus = false;
+  toggleBtn: 'Enable' | 'Disable';
+
   // CTOR
   constructor(
     private authService:AuthService,
@@ -27,5 +30,54 @@ export class NotificationsTab {
         this.notificationService.getNotifications();
       }
     })
+
+    // if push notifications is enabled, have the button show 'disable'
+    if(this.pushStatus) {
+      this.toggleBtn = 'Disable';
+    }
+    // otherwise show 'enable'
+    else {
+      this.toggleBtn = 'Enable';
+    }
+  }
+
+  /*
+  Function Name: ngOnChanges()
+  Function Description: This method is automatically triggered by Angular upon
+                        changes in parent component. It checks whether push notifications
+                        are currently enabled.
+  Parameters: None.
+  ----------------
+  Programmer: Shir Bar Lev.
+  */
+  ngOnChanges() {
+    // if push notifications is enabled, have the button show 'disable'
+    if(this.pushStatus) {
+      this.toggleBtn = 'Disable';
+    }
+    // otherwise show 'enable'
+    else {
+      this.toggleBtn = 'Enable';
+    }
+  }
+
+  /*
+  Function Name: togglePushNotifications()
+  Function Description: Enables and disables push notifications.
+  Parameters: None.
+  ----------------
+  Programmer: Shir Bar Lev.
+  */
+  togglePushNotifications() {
+    // if notifications are enabled, disable them
+    if(this.pushStatus) {
+      this.pushStatus = false;
+      this.notificationService.unsubscribeFromStream();
+    }
+    // otherwise enable them
+    else {
+      this.pushStatus = true;
+      this.notificationService.subscribeToStream();
+    }
   }
 }

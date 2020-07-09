c8d12d62dfab39fca414a7ee69eeb5f211613fbb
Shir Bar Lev <shirblc@gmail.com>
2020-07-09 16:21:21 +0300

    Fixed an error in environment config script.

--- env-config.js
+++ env-config.js
@@ -5,7 +5,7 @@ fs.readFile('dist/app.bundle.min.js', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  var result = data.replace(/{production:e.env.production,auth0:{domain:e.env.auth0domain,clientID:e.env.auth0client,audience:e.env.audience,redirectUri:e.env.loginurl,logoutUri:e.env.logouturl},backend:{domain:e.env.apiurl},vapidKey:''}/g,
+  var result = data.replace(/{production:e.env.production,auth0:{domain:e.env.auth0domain,clientID:e.env.auth0client,audience:e.env.audience,redirectUri:e.env.loginurl,logoutUri:e.env.logouturl},backend:{domain:e.env.apiurl},vapidKey:""}/g,
     `{production:'${process.env.PRODUCTION}',auth0:{domain:'${process.env.AUTH0_DOMAIN}',clientID:'${process.env.AUTH0_CLIENT}',audience:'${process.env.AUDIENCE}',redirectUri:'${process.env.LOGIN_REDIRECT}',logoutUri:'${process.env.LOGOUT_REDIRECT}'},backend:{domain:'${process.env.BACKEND_URL}'},vapidKey:'${process.env.PUBLIC_KEY}'}`);
 
   fs.writeFile('dist/app.bundle.min.js', result, 'utf8', function (err) {

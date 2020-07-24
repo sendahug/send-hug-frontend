ebd226212e751cd9a7e411a6b0ee297c74434944
Shir Bar Lev <shirblc@gmail.com>
2020-07-24 14:41:44 +0300

    Added notifications-related settings.

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -20,4 +20,64 @@ export class SettingsPage {
   ) {
 
   }
+
+  /*
+  Function Name: togglePushNotifications()
+  Function Description: Enables and disables push notifications.
+  Parameters: None.
+  ----------------
+  Programmer: Shir Bar Lev.
+  */
+  togglePushNotifications() {
+    // if notifications are enabled, disable them
+    if(this.notificationService.pushStatus) {
+      this.notificationService.pushStatus = false;
+      this.notificationService.updateUserSettings();
+      this.notificationService.unsubscribeFromStream();
+    }
+    // otherwise enable them
+    else {
+      this.notificationService.pushStatus = true;
+      this.notificationService.updateUserSettings();
+      this.notificationService.subscribeToStream();
+    }
+  }
+
+  /*
+  Function Name: togglePushNotifications()
+  Function Description: Enables and disables automatically refreshing the user's
+                        notifications in the background.
+  Parameters: None.
+  ----------------
+  Programmer: Shir Bar Lev.
+  */
+  toggleAutoRefresh() {
+    // if auto-refresh is enabled, disable it
+    if(this.notificationService.refreshStatus) {
+      this.notificationService.refreshStatus = false;
+      this.notificationService.refreshRateSecs = 0;
+      this.notificationService.updateUserSettings();
+      this.notificationService.stopAutoRefresh();
+    }
+    // otherwise enable it
+    else {
+      this.notificationService.refreshStatus = true;
+      this.notificationService.refreshRateSecs = 20;
+      this.notificationService.updateUserSettings();
+      this.notificationService.startAutoRefresh();
+    }
+  }
+
+  /*
+  Function Name: updateRefreshRate()
+  Function Description: Updates the user's auto-refersh rate.
+  Parameters: e (event) - the button click that triggers the method.
+              newRate (number) - the new refresh rate in seconds.
+  ----------------
+  Programmer: Shir Bar Lev.
+  */
+  updateRefreshRate(e:Event, newRate:number) {
+    e.preventDefault();
+    this.notificationService.refreshRateSecs = newRate;
+  }
 }

ed06162e720776bdb79d3bf6de28fa35125c8065
Shir Bar Lev <shirblc@gmail.com>
2020-08-02 13:36:56 +0300

    Added main page specs file.

---
+++ src/app/components/mainPage/mainPage.component.spec.ts
@@ -0,0 +1,47 @@
+import { fakeAsync, TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from '@angular/router/testing';
+import {} from 'jasmine';
+import { APP_BASE_HREF } from '@angular/common';
+
+import { AppComponent } from '../../app.component';
+import { MainPage } from "./mainPage.component";
+import { Loader } from '../loader/loader.component';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from "@angular/platform-browser-dynamic/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { ServiceWorkerModule } from "@angular/service-worker";
+
+describe('MainPage', () => {
+
+  // Before each test, configure testing environment
+  beforeEach(fakeAsync(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+        platformBrowserDynamicTesting());
+
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        ServiceWorkerModule.register('sw.js')
+      ],
+      declarations: [
+        AppComponent,
+        MainPage,
+        Loader
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' }
+      ]
+    }).compileComponents();
+  }));
+
+  // Check that the component is created
+  it('should create the component', fakeAsync(() => {
+    const fixture = TestBed.createComponent(MainPage);
+    const mainPage = fixture.componentInstance;
+    expect(mainPage).toBeTruthy();
+  }));
+})

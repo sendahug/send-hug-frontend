7359af7e0b9e36723fe27d77da17132e3f5b8e01
Shir Bar Lev <shirblc@gmail.com>
2020-08-06 14:29:02 +0300

    Added settings page specs file.

---
+++ src/app/components/settings/settings.component.spec.ts
@@ -0,0 +1,47 @@
+import 'zone.js/dist/zone';
+import "zone.js/dist/proxy";
+import "zone.js/dist/sync-test";
+import "zone.js/dist/jasmine-patch";
+import "zone.js/dist/async-test";
+import "zone.js/dist/fake-async-test";
+import { TestBed, tick, fakeAsync } from "@angular/core/testing";
+import { RouterTestingModule } from '@angular/router/testing';
+import {} from 'jasmine';
+import { APP_BASE_HREF } from '@angular/common';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from "@angular/platform-browser-dynamic/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { ServiceWorkerModule } from "@angular/service-worker";
+
+import { AppComponent } from '../../app.component';
+import { SettingsPage } from './settings.component';
+import { NotificationService } from '../../services/notifications.service';
+import { MockNotificationService } from '../../services/notifications.service.mock';
+
+
+describe('SettingsPage', () => {
+  // Before each test, configure testing environment
+  beforeEach(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+        platformBrowserDynamicTesting());
+
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        ServiceWorkerModule.register('sw.js')
+      ],
+      declarations: [
+        AppComponent,
+        SettingsPage
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        { provide: NotificationService, useClass: MockNotificationService }
+      ]
+    }).compileComponents();
+  });
+});

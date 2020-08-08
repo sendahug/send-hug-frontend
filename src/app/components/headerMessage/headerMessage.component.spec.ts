bbc07b36b4b887296356fd3479420bd582892244
Shir Bar Lev <shirblc@gmail.com>
2020-08-08 20:53:57 +0300

    Added HeaderMessage specs file.

---
+++ src/app/components/headerMessage/headerMessage.component.spec.ts
@@ -0,0 +1,60 @@
+import 'zone.js/dist/zone';
+import "zone.js/dist/proxy";
+import "zone.js/dist/sync-test";
+import "zone.js/dist/jasmine-patch";
+import "zone.js/dist/async-test";
+import "zone.js/dist/fake-async-test";
+import { TestBed, tick, fakeAsync } from "@angular/core/testing";
+import { RouterTestingModule } from '@angular/router/testing';
+import {} from 'jasmine';
+import { APP_BASE_HREF } from '@angular/common';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from "@angular/platform-browser-dynamic/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { ServiceWorkerModule } from "@angular/service-worker";
+
+import { AppComponent } from '../../app.component';
+import { HeaderMessage } from './headerMessage.component';
+import { AuthService } from '../../services/auth.service';
+import { MockAuthService } from '../../services/auth.service.mock';
+import { ItemsService } from '../../services/items.service';
+import { MockItemsService } from '../../services/items.service.mock';
+import { BehaviorSubject } from 'rxjs';
+
+describe('HeaderMessage', () => {
+  // Before each test, configure testing environment
+  beforeEach(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+        platformBrowserDynamicTesting());
+
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        ServiceWorkerModule.register('sw.js')
+      ],
+      declarations: [
+        AppComponent,
+        HeaderMessage
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        { provide: AuthService, useClass: MockAuthService },
+        { provide: ItemsService, useClass: MockItemsService }
+      ]
+    }).compileComponents();
+  });
+
+  // Check that the component is created
+  it('should create the component', () => {
+    const acFixture = TestBed.createComponent(AppComponent);
+    const appComponent = acFixture.componentInstance;
+    const fixture = TestBed.createComponent(HeaderMessage);
+    const headerMessage  = fixture.componentInstance;
+    expect(appComponent).toBeTruthy();
+    expect(headerMessage).toBeTruthy();
+  });
+});

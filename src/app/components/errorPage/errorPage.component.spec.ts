5dcb4820326cee5de1b2b514d7551cf82c9085c6
Shir Bar Lev <shirblc@gmail.com>
2020-08-08 18:41:43 +0300

    Added ErrorPage specs file.

---
+++ src/app/components/errorPage/errorPage.component.spec.ts
@@ -0,0 +1,54 @@
+import 'zone.js/dist/zone';
+import "zone.js/dist/proxy";
+import "zone.js/dist/sync-test";
+import "zone.js/dist/jasmine-patch";
+import "zone.js/dist/async-test";
+import "zone.js/dist/fake-async-test";
+import { TestBed, tick, fakeAsync } from "@angular/core/testing";
+import { RouterTestingModule } from '@angular/router/testing';
+import {} from 'jasmine';
+import { APP_BASE_HREF } from '@angular/common';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from "@angular/platform-browser-dynamic/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { ServiceWorkerModule } from "@angular/service-worker";
+
+// App imports
+import { AppComponent } from '../../app.component';
+import { ErrorPage } from "./errorPage.component";
+
+describe('ErrorPage', () => {
+  // Before each test, configure testing environment
+  beforeEach(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+        platformBrowserDynamicTesting());
+
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        ServiceWorkerModule.register('sw.js')
+      ],
+      declarations: [
+        AppComponent,
+        ErrorPage
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' }
+      ]
+    }).compileComponents();
+  });
+
+  // Check that the component is created
+  it('should create the component', () => {
+    const acFixture = TestBed.createComponent(AppComponent);
+    const appComponent = acFixture.componentInstance;
+    const fixture = TestBed.createComponent(ErrorPage);
+    const errorPage = fixture.componentInstance;
+    expect(appComponent).toBeTruthy();
+    expect(errorPage).toBeTruthy();
+  });
+});

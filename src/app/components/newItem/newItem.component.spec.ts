5520f017f659d67b4e112d4479c5807fd3828316
Shir Bar Lev <shiruchii@gmail.com>
2020-08-08 13:59:57 +0300

    Added NewItem component specs file.

---
+++ src/app/components/newItem/newItem.component.spec.ts
@@ -0,0 +1,56 @@
+import 'zone.js/dist/zone';
+import "zone.js/dist/proxy";
+import "zone.js/dist/sync-test";
+import "zone.js/dist/jasmine-patch";
+import "zone.js/dist/async-test";
+import "zone.js/dist/fake-async-test";
+import { TestBed, tick, fakeAsync } from "@angular/core/testing";
+import { RouterTestingModule } from '@angular/router/testing';
+import {} from 'jasmine';
+import { APP_BASE_HREF } from '@angular/common';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from "@angular/platform-browser-dynamic/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { ServiceWorkerModule } from "@angular/service-worker";
+
+import { AppComponent } from '../../app.component';
+import { NewItem } from './newItem.component';
+import { PostsService } from '../../services/posts.service';
+import { MockPostsService } from '../../services/posts.service.mock';
+import { AuthService } from '../../services/auth.service';
+import { MockAuthService } from '../../services/auth.service.mock';
+import { ItemsService } from '../../services/items.service';
+import { MockItemsService } from '../../services/items.service.mock';
+import { AlertsService } from '../../services/alerts.service';
+import { MockAlertsService } from '../../services/alerts.service.mock';
+import { ActivatedRoute } from "@angular/router";
+
+describe('NewItem', () => {
+  // Before each test, configure testing environment
+  beforeEach(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+        platformBrowserDynamicTesting());
+
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        ServiceWorkerModule.register('sw.js')
+      ],
+      declarations: [
+        AppComponent,
+        NewItem
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        { provide: PostsService, useClass: MockPostsService },
+        { provide: AuthService, useClass: MockAuthService },
+        { provide: ItemsService, useClass: MockItemsService },
+        { provide: AlertsService, useClass: MockAlertsService }
+      ]
+    }).compileComponents();
+  });
+});

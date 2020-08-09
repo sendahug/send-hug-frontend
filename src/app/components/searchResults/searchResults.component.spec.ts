9c8bb904f63ecd953f6daa91a6106a167081239f
Shir Bar Lev <shiruchii@gmail.com>
2020-08-09 16:33:38 +0300

    Add SearchResults component specs file.

---
+++ src/app/components/searchResults/searchResults.component.spec.ts
@@ -0,0 +1,65 @@
+import 'zone.js/dist/zone';
+import "zone.js/dist/proxy";
+import "zone.js/dist/sync-test";
+import "zone.js/dist/jasmine-patch";
+import "zone.js/dist/async-test";
+import "zone.js/dist/fake-async-test";
+import { TestBed, tick, fakeAsync } from "@angular/core/testing";
+import { RouterTestingModule } from '@angular/router/testing';
+import {} from 'jasmine';
+import { APP_BASE_HREF } from '@angular/common';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from "@angular/platform-browser-dynamic/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { ActivatedRoute } from "@angular/router";
+import { BehaviorSubject } from 'rxjs';
+
+import { AppComponent } from '../../app.component';
+import { SearchResults } from './searchResults.component';
+import { PopUp } from '../popUp/popUp.component';
+import { Loader } from '../loader/loader.component';
+import { ItemsService } from '../../services/items.service';
+import { MockItemsService } from '../../services/items.service.mock';
+import { AuthService } from '../../services/auth.service';
+import { MockAuthService } from '../../services/auth.service.mock';
+
+describe('SearchResults', () => {
+  // Before each test, configure testing environment
+  beforeEach(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+        platformBrowserDynamicTesting());
+
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        ServiceWorkerModule.register('sw.js')
+      ],
+      declarations: [
+        AppComponent,
+        SearchResults,
+        PopUp,
+        Loader
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        { provide: ItemsService, useClass: MockItemsService },
+        { provide: AuthService, useClass: MockAuthService }
+      ]
+    }).compileComponents();
+  });
+
+  // Check that the component is created
+  it('should create the component', () => {
+    const acFixture = TestBed.createComponent(AppComponent);
+    const appComponent = acFixture.componentInstance;
+    const fixture = TestBed.createComponent(SearchResults);
+    const searchResults = fixture.componentInstance;
+    expect(appComponent).toBeTruthy();
+    expect(searchResults).toBeTruthy();
+  });
+});

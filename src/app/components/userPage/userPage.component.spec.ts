2b6534e4ae405a912fac34acc51bbab59fddffc3
Shir Bar Lev <shiruchii@gmail.com>
2020-08-09 10:54:51 +0300

    Added UserPage specs file.

---
+++ src/app/components/userPage/userPage.component.spec.ts
@@ -0,0 +1,66 @@
+import 'zone.js/dist/zone';
+import "zone.js/dist/proxy";
+import "zone.js/dist/sync-test";
+import "zone.js/dist/jasmine-patch";
+import "zone.js/dist/async-test";
+import "zone.js/dist/fake-async-test";
+import { TestBed, tick, fakeAsync } from "@angular/core/testing";
+import { RouterTestingModule } from '@angular/router/testing';
+import {} from 'jasmine';
+import { APP_BASE_HREF } from '@angular/common';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from "@angular/platform-browser-dynamic/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { ActivatedRoute } from "@angular/router";
+
+import { AppComponent } from '../../app.component';
+import { UserPage } from './userPage.component';
+import { PopUp } from '../popUp/popUp.component';
+import { Loader } from '../loader/loader.component';
+import { MyPosts } from '../myPosts/myPosts.component';
+import { ItemsService } from '../../services/items.service';
+import { MockItemsService } from '../../services/items.service.mock';
+import { AuthService } from '../../services/auth.service';
+import { MockAuthService } from '../../services/auth.service.mock';
+
+describe('UserPage', () => {
+  // Before each test, configure testing environment
+  beforeEach(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+        platformBrowserDynamicTesting());
+
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        ServiceWorkerModule.register('sw.js')
+      ],
+      declarations: [
+        AppComponent,
+        UserPage,
+        PopUp,
+        Loader,
+        MyPosts
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        { provide: ItemsService, useClass: MockItemsService },
+        { provide: AuthService, useClass: MockAuthService }
+      ]
+    }).compileComponents();
+  });
+
+  // Check that the component is created
+  it('should create the component', () => {
+    const acFixture = TestBed.createComponent(AppComponent);
+    const appComponent = acFixture.componentInstance;
+    const fixture = TestBed.createComponent(UserPage);
+    const userPage = fixture.componentInstance;
+    expect(appComponent).toBeTruthy();
+    expect(userPage).toBeTruthy();
+  });
+});

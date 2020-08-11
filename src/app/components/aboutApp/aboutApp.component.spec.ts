2b3a5d952548282db4b75ff5e58f31896179b198
Shir Bar Lev <shiruchii@gmail.com>
2020-08-11 13:05:49 +0300

    Added AboutApp spec.

---
+++ src/app/components/aboutApp/aboutApp.component.spec.ts
@@ -0,0 +1,47 @@
+import { TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from '@angular/router/testing';
+import {} from 'jasmine';
+import { APP_BASE_HREF } from '@angular/common';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from "@angular/platform-browser-dynamic/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { ServiceWorkerModule } from "@angular/service-worker";
+
+import { AppComponent } from "../../app.component";
+import { AboutApp } from "./aboutApp.component";
+
+describe('AboutApp', () => {
+  // Before each test, configure testing environment
+  beforeEach(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+        platformBrowserDynamicTesting());
+
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        ServiceWorkerModule.register('sw.js')
+      ],
+      declarations: [
+        AppComponent,
+        AboutApp
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' }
+      ]
+    }).compileComponents();
+  });
+
+  // Check the page is created
+  it('should create the component', () => {
+    const acFixture = TestBed.createComponent(AppComponent);
+    const appComponent = acFixture.componentInstance;
+    const fixture = TestBed.createComponent(AboutApp);
+    const aboutApp = fixture.componentInstance;
+    expect(appComponent).toBeTruthy();
+    expect(aboutApp).toBeTruthy();
+  });
+});

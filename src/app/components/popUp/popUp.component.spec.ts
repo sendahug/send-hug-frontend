0f4d4cd20ac2546f70461c6faecf988b029df25b
Shir Bar Lev <shiruchii@gmail.com>
2020-08-11 12:44:27 +0300

    Added popup specs file.

---
+++ src/app/components/popUp/popUp.component.spec.ts
@@ -0,0 +1,68 @@
+import 'zone.js/dist/zone';
+import "zone.js/dist/proxy";
+import "zone.js/dist/sync-test";
+import "zone.js/dist/jasmine-patch";
+import "zone.js/dist/async-test";
+import "zone.js/dist/fake-async-test";
+import { TestBed, tick, fakeAsync } from "@angular/core/testing";
+import { RouterTestingModule } from '@angular/router/testing';
+import {} from 'jasmine';
+import { APP_BASE_HREF } from '@angular/common';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from "@angular/platform-browser-dynamic/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { ServiceWorkerModule } from "@angular/service-worker";
+
+import { AppComponent } from '../../app.component';
+import { PopUp } from './popUp.component';
+import { AuthService } from '../../services/auth.service';
+import { MockAuthService } from '../../services/auth.service.mock';
+import { ItemsService } from '../../services/items.service';
+import { MockItemsService } from '../../services/items.service.mock';
+import { PostsService } from '../../services/posts.service';
+import { MockPostsService } from '../../services/posts.service.mock';
+import { AdminService } from '../../services/admin.service';
+import { MockAdminService } from '../../services/admin.service.mock';
+import { AlertsService } from '../../services/alerts.service';
+import { MockAlertsService } from '../../services/alerts.service.mock';
+
+describe('Popup', () => {
+  // Before each test, configure testing environment
+  beforeEach(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+        platformBrowserDynamicTesting());
+
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        ServiceWorkerModule.register('sw.js')
+      ],
+      declarations: [
+        AppComponent,
+        PopUp
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        { provide: PostsService, useClass: MockPostsService },
+        { provide: AuthService, useClass: MockAuthService },
+        { provide: ItemsService, useClass: MockItemsService },
+        { provide: AdminService, useClass: MockAdminService },
+        { provide: AlertsService, useClass: MockAlertsService }
+      ]
+    }).compileComponents();
+  });
+
+  // Check that the component is created
+  it('should create the component', () => {
+    const acFixture = TestBed.createComponent(AppComponent);
+    const appComponent = acFixture.componentInstance;
+    const fixture = TestBed.createComponent(PopUp);
+    const popUp = fixture.componentInstance;
+    expect(appComponent).toBeTruthy();
+    expect(popUp).toBeTruthy();
+  });
+});

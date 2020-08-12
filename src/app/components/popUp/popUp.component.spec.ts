7198df9291526499e991f682177bf14a044026b6
Shir Bar Lev <shiruchii@gmail.com>
2020-08-12 13:49:51 +0300

    Added item delete tests (popup component).

--- src/app/components/popUp/popUp.component.spec.ts
+++ src/app/components/popUp/popUp.component.spec.ts
@@ -717,4 +717,192 @@ describe('Popup', () => {
       expect(updateServiceSpy).not.toHaveBeenCalled();
     }));
   });
+
+  // DELETE
+  // ==================================================================
+  describe('Item Delete', () => {
+    // Before each test, configure testing environment
+    beforeEach(() => {
+      TestBed.resetTestEnvironment();
+      TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+          platformBrowserDynamicTesting());
+
+      TestBed.configureTestingModule({
+        imports: [
+          RouterTestingModule,
+          HttpClientModule,
+          ServiceWorkerModule.register('sw.js')
+        ],
+        declarations: [
+          AppComponent,
+          PopUp
+        ],
+        providers: [
+          { provide: APP_BASE_HREF, useValue: '/' },
+          { provide: PostsService, useClass: MockPostsService },
+          { provide: AuthService, useClass: MockAuthService },
+          { provide: ItemsService, useClass: MockItemsService },
+          { provide: AdminService, useClass: MockAdminService },
+          { provide: AlertsService, useClass: MockAlertsService }
+        ]
+      }).compileComponents();
+    });
+
+    // Check that a warning is shown before deleting an item
+    it('shows a warning when deleting something', fakeAsync(() => {
+      TestBed.createComponent(AppComponent);
+      TestBed.get(AuthService).login();
+      const fixture = TestBed.createComponent(PopUp);
+      const popUp = fixture.componentInstance;
+      const popUpDOM = fixture.nativeElement;
+      popUp.toDelete = 'Post';
+      popUp.delete = true;
+      popUp.report = false;
+      popUp.itemToDelete = 2;
+
+      fixture.detectChanges();
+      tick();
+
+      expect(popUpDOM.querySelector('#deleteItem')).toBeTruthy();
+      expect(popUpDOM.querySelector('#deleteItem').querySelectorAll('.warning')[0]).toBeTruthy();
+      expect(popUpDOM.querySelector('#deleteItem').querySelectorAll('.warning')[0].textContent).toContain('This action is irreversible!');
+    }));
+
+    // Check that the correct method is called depending on the item that's being deleted
+    it('calls the correct method upon confirmation', fakeAsync(() => {
+      TestBed.createComponent(AppComponent);
+      TestBed.get(AuthService).login();
+      const fixture = TestBed.createComponent(PopUp);
+      const popUp = fixture.componentInstance;
+      const popUpDOM = fixture.nativeElement;
+      popUp.delete = true;
+      popUp.report = false;
+      const deleteItems = [
+        'Post', 'Message', 'Thread', 'All posts', 'All inbox', 'All outbox', 'All threads'
+      ];
+      const requredArgs = [
+        1, [1, 'inbox'], [2, 'thread'], 4, ['All inbox', 4], ['All outbox', 4], ['All threads', 4]
+      ];
+      let methodSpies = [
+        spyOn(popUp['postsService'], 'deletePost').and.callThrough(),
+        spyOn(popUp['itemsService'], 'deleteMessage').and.callThrough(),
+        spyOn(popUp['itemsService'], 'deleteThread').and.callThrough(),
+        spyOn(popUp['postsService'], 'deleteAllPosts').and.callThrough(),
+        spyOn(popUp['itemsService'], 'deleteAll').and.callThrough(),
+        spyOn(popUp['itemsService'], 'deleteAll').and.callThrough(),
+        spyOn(popUp['itemsService'], 'deleteAll').and.callThrough()
+      ];
+      let currentSpy: jasmine.Spy;
+      let calledSpies: jasmine.Spy[] = [];
+
+      deleteItems.forEach((item) => {
+        // set up the popup
+        popUp.toDelete = item;
+        popUp.itemToDelete = 1;
+        if(popUp.toDelete == 'Message') {
+          popUp.messType = 'inbox';
+        }
+        else if(popUp.toDelete == 'Thread') {
+          popUp.messType = 'thread';
+        }
+
+        currentSpy = methodSpies.shift()!;
+        calledSpies.push(currentSpy);
+
+        popUpDOM.querySelectorAll('.popupDeleteBtn')[0].click();
+        fixture.detectChanges();
+        tick();
+
+        // check the other spies weren't called
+        methodSpies.forEach((spy) => {
+          expect(spy).not.toHaveBeenCalled();
+        });
+
+        // check the current and previous spies were each called once
+        calledSpies.forEach((spy) => {
+          expect(spy).toHaveBeenCalled();
+          expect(spy).toHaveBeenCalledTimes(1);
+        });
+      });
+    }));
+
+    // Check that a request to close the report is made if the item is deleted from
+    // the admin dashboard
+    it('makes a request to close the report if that\'s what the user chose - Admin delete', fakeAsync(() => {
+      TestBed.createComponent(AppComponent);
+      TestBed.get(AuthService).login();
+      const fixture = TestBed.createComponent(PopUp);
+      const popUp = fixture.componentInstance;
+      const popUpDOM = fixture.nativeElement;
+      popUp.toDelete = 'ad post';
+      popUp.delete = true;
+      popUp.report = false;
+      popUp.itemToDelete = 2;
+      popUp.reportData = {
+        reportID: 4,
+        postID: 4
+      };
+      const deleteSpy = spyOn(popUp, 'deletePost').and.callThrough();
+      const deleteServiceSpy = spyOn(popUp['adminService'], 'deletePost').and.callThrough();
+
+      fixture.detectChanges();
+      tick();
+
+      // click 'delete and close report'
+      popUpDOM.querySelectorAll('.popupDeleteBtn')[0].click();
+      fixture.detectChanges();
+      tick();
+
+      // check that the closeReport boolean is true
+      const report = {
+        reportID: 4,
+        postID: 4
+      };
+      expect(deleteSpy).toHaveBeenCalled();
+      expect(deleteSpy).toHaveBeenCalledWith(true);
+      expect(deleteServiceSpy).toHaveBeenCalled();
+      expect(deleteServiceSpy).toHaveBeenCalledWith(2, report, true);
+
+      // click 'delete and close report'
+      popUpDOM.querySelectorAll('.popupDeleteBtn')[1].click();
+      fixture.detectChanges();
+      tick();
+
+      // check that the closeReport boolean is false
+      expect(deleteSpy).toHaveBeenCalled();
+      expect(deleteSpy).toHaveBeenCalledWith(false);
+      expect(deleteServiceSpy).toHaveBeenCalled();
+      expect(deleteServiceSpy).toHaveBeenCalledWith(2, report, false);
+    }));
+
+    // Check that the popup is exited and the item isn't deleted if the user picks 'never mind'
+    it('should emit false and keep the item if the user chooses not to delete', fakeAsync(() => {
+      TestBed.createComponent(AppComponent);
+      TestBed.get(AuthService).login();
+      const fixture = TestBed.createComponent(PopUp);
+      const popUp = fixture.componentInstance;
+      const popUpDOM = fixture.nativeElement;
+      popUp.toDelete = 'Post';
+      popUp.delete = true;
+      popUp.report = false;
+      popUp.itemToDelete = 2;
+      const exitSpy = spyOn(popUp, 'exitEdit').and.callThrough();
+      const deleteSpy = spyOn(popUp, 'deleteItem').and.callThrough();
+
+      fixture.detectChanges();
+      tick();
+
+      // click the 'never mind button'
+      popUpDOM.querySelectorAll('.popupDeleteBtn')[1].click();
+      fixture.detectChanges();
+      tick();
+
+      // check the exit method was called
+      popUp.editMode.subscribe((event:boolean) => {
+        expect(event).toBeFalse();
+      });
+      expect(exitSpy).toHaveBeenCalled();
+      expect(deleteSpy).not.toHaveBeenCalled();
+    }));
+  });
 });

fad51db7b844ec1a182325655d1921d60d5cf020
Shir Bar Lev <shirblc@gmail.com>
2020-08-16 11:34:44 +0300

    Added copyright notices to all components and specs files.

--- src/app/components/messages/messages.component.spec.ts
+++ src/app/components/messages/messages.component.spec.ts
@@ -1,3 +1,22 @@
+/*
+	Messages Page
+	Send a Hug Component Tests
+---------------------------------------------------
+MIT License
+
+Copyright (c) 2020 Send A Hug
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+*/
+
 import 'zone.js/dist/zone';
 import "zone.js/dist/proxy";
 import "zone.js/dist/sync-test";

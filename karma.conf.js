44eb05a57c2680d96d8b00ac080a397af69d7161
Shir Bar Lev <shiruchii@gmail.com>
2020-09-20 15:20:57 +0300

    Replaced webpack with browserify in Karma.

--- karma.conf.js
+++ karma.conf.js
@@ -1,88 +1,39 @@
 const path = require('path');
-const ngTools = require('@ngtools/webpack');
 process.env.CHROME_BIN = '/usr/bin/google-chrome-stable';
 
 // Karma configuration file
 module.exports = function (karma) {
   karma.set({
     basePath: '',
-    frameworks: ['jasmine'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('karma-webpack'),
-      require('karma-coverage'),
-      require('karma-sourcemap-loader')
-    ],
+    frameworks: ['jasmine', 'browserify'],
     mime: { 'text/x-typescript': ['ts','tsx'] },
     files: [
         { pattern: "./src/**/*.spec.ts" },
         { pattern: "./tests/app.js" }
     ],
     preprocessors: {
-        "./tests/app.js": ['sourcemap', 'coverage'],
-        './src/**/*.spec.ts': ['webpack', 'sourcemap', 'coverage']
+        "./tests/src/**/*.spec.ts" : ['browserify'],
+        './tests/app.bundle.js': ['sourcemap']
     },
-    webpack: {
-        devtool: "eval-source-map",
-        entry: {
-          app: './src/main.ts',
-          test: './src/tests.ts'
-        },
-        mode: "development",
-        node: { fs: 'empty' },
-        module: {
-            rules: [
-                {
-                    test: /\.html$/,
-                    loader: 'html-loader'
-                },
-                {
-                    test: /\.svg$/,
-                    loader: 'svg-inline-loader'
-                },
-                {
-                  test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
-                  loader: [
-                    '@ngtools/webpack',
-                    { loader: 'angular-router-loader' }
-                  ]
-                }
-            ]
-        },
-        plugins: [
-          new ngTools.AngularCompilerPlugin({
-            tsConfigPath: 'tsconfig.json',
-            basePath: './',
-            entryModule: path.resolve(__dirname, 'src/app/app.module#AppModule'),
-            skipCodeGeneration: true,
-            sourceMap: true,
-            directTemplateLoading: false,
-            locale: 'en',
-            hostReplacementPaths: {
-              'src/environments/config.development.ts': 'src/environments/config.production.ts'
-            }
-          })
-        ]
+    browserify: {
+      debug: true,
+      plugin: ['tsify'],
+      extensions: ['ts', 'tsx'],
+      configure: function(bundle) {
+        bundle.on('prebundle', function() {
+          bundle.transform('tsify', { target: 'es6' });
+        });
+      }
     },
     coverageIstanbulReporter: {
       dir: path.resolve(__dirname, './coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    coverageReporter: {
-      reports: [
-        { type: 'html', subdir: 'report-html' },
-        { type: 'lcov', subdir: 'report-lcov' }
-      ],
-      dir : 'coverage/'
-    },
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    reporters: ['progress', 'kjhtml', 'coverage', 'coverage-istanbul'],
+    reporters: ['progress', 'kjhtml', 'coverage-istanbul'],
     port: 9876,
     logLevel: 'DEBUG',
     autoWatch: false,

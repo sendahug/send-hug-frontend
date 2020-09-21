a15521ffedc53b09e6fad357d3d01a9a8fe0f9fb
Shir Bar Lev <shirblc@gmail.com>
2020-09-21 15:59:23 +0300

    Replaced require.context with a placeholder for test files.

--- src/tests.ts
+++ src/tests.ts
@@ -4,21 +4,10 @@ import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-
-declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
-};
+// test-placeholder
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-
-// And load the modules.
-context.keys().map(context);

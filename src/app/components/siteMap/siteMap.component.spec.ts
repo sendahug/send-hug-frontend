c1747d9c17c7b2dd6306f8888e8aff418b835d8d
Shir Bar Lev <shirblc@gmail.com>
2020-09-25 19:19:33 +0300

    Added more SiteMap tests.

--- src/app/components/siteMap/siteMap.component.spec.ts
+++ src/app/components/siteMap/siteMap.component.spec.ts
@@ -34,6 +34,8 @@ import { Routes } from "@angular/router";
 import { AppComponent } from "../../app.component";
 import { SiteMap } from "./siteMap.component";
 import { NotificationsTab } from '../notifications/notifications.component';
+import { AuthService } from '../../services/auth.service';
+import { MockAuthService } from '../../services/auth.service.mock';
 
 // Mock Component for testing the sitemap
 // ==================================================
@@ -65,10 +67,26 @@ export const routes: Routes = [
         { path: ':id', pathMatch: 'prefix', component: MockComp, data: { name: 'Other User\'s Page' } }
       ], data: { name: 'User Page' }},
   { path: 'settings', component: MockComp, data: { name: 'Settings Page' } },
-  { path: 'sitemap', component: SiteMap, data: { name: 'Site Map' } }
+  { path: 'sitemap', component: SiteMap, data: { name: 'Site Map' } },
+  { path: '**', component: MockComp, data: { name: 'Error Page' } },
+  { path: 'admin',
+      children: [
+        { path: '', pathMatch: 'prefix', component: MockComp, data: { name: 'Main Page' } },
+        { path: 'reports', pathMatch: 'prefix', component: MockComp, data: { name: 'Reports Page' } },
+        { path: 'blocks', pathMatch: 'prefix', component: MockComp, data: { name: 'Blocks Page' } },
+        { path: 'filters', pathMatch: 'prefix', component: MockComp, data: { name: 'Filters Page' } }
+      ], data: { name: 'Admin Dashboard' }},
+  { path: 'messages',
+      children: [
+        { path: '', pathMatch: 'prefix', redirectTo: 'inbox', data: { name: 'Inbox' } },
+        { path: 'inbox', pathMatch: 'prefix', component: MockComp, data: { name: 'Inbox' } },
+        { path: 'outbox', pathMatch: 'prefix', component: MockComp, data: { name: 'Outbox' } },
+        { path: 'threads', pathMatch: 'prefix', component: MockComp, data: { name: 'Threads' } },
+        { path: 'thread/:id', pathMatch: 'prefix', component: MockComp, data: { name: 'Thread' } }
+      ], data: { name: 'Mailbox' }}
 ];
 
-describe('AboutApp', () => {
+describe('SiteMap', () => {
   // Before each test, configure testing environment
   beforeEach(() => {
     TestBed.resetTestEnvironment();
@@ -89,7 +107,8 @@ describe('AboutApp', () => {
         SiteMap
       ],
       providers: [
-        { provide: APP_BASE_HREF, useValue: '/' }
+        { provide: APP_BASE_HREF, useValue: '/' },
+        { provide: AuthService, useClass: MockAuthService }
       ]
     }).compileComponents();
   });
@@ -123,6 +142,136 @@ describe('AboutApp', () => {
       expect(navLinks[i]).toBeDefined();
       expect(navLinks[i]!.getAttribute('href')).toBeDefined();
       expect(navLinks[i]!.getAttribute('href')).not.toBe('');
+    }
+  });
+
+  // Check that the admin board links are shown if the user has permission
+  it('should show admin board links if the user has permission', () => {
+    const authSpy = spyOn(TestBed.inject(AuthService), 'canUser').and.returnValue(true);
+    TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(SiteMap);
+    const siteMap = fixture.componentInstance;
+    const siteMapDOM = fixture.nativeElement;
+    fixture.detectChanges();
+
+    let routeList = siteMapDOM.querySelector('#routeList');
+    let navLinks = routeList!.querySelectorAll('.routerLink');
+    let adminPath = { path: 'admin',
+        children: [
+          { path: '', pathMatch: 'prefix', component: MockComp, data: { name: 'Main Page' } },
+          { path: 'reports', pathMatch: 'prefix', component: MockComp, data: { name: 'Reports Page' } },
+          { path: 'blocks', pathMatch: 'prefix', component: MockComp, data: { name: 'Blocks Page' } },
+          { path: 'filters', pathMatch: 'prefix', component: MockComp, data: { name: 'Filters Page' } }
+        ], data: { name: 'Admin Dashboard' }};
+
+    // check the admin pages' linkes appear
+    expect(authSpy).toHaveBeenCalled();
+    expect(siteMap.routes).toContain(adminPath);
+    expect(navLinks[4].textContent).toBe('Main Page');
+    expect(navLinks[4].parentElement.parentElement.children.length).toBe(4);
+    expect(navLinks[4].parentElement.parentElement.parentElement.firstElementChild.textContent).toBe('Admin Dashboard');
+  });
+
+  // Check that the admin board links aren't shown if the user doesn't have permission
+  it('should hide admin board links if the user doesn\'t have permission', () => {
+    const authSpy = spyOn(TestBed.inject(AuthService), 'canUser').and.returnValue(false);
+    TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(SiteMap);
+    const siteMap = fixture.componentInstance;
+    const siteMapDOM = fixture.nativeElement;
+    fixture.detectChanges();
+
+    let routeList = siteMapDOM.querySelector('#routeList');
+    let navLinks = routeList!.querySelectorAll('.routerLink');
+    let adminPath = { path: 'admin',
+        children: [
+          { path: '', pathMatch: 'prefix', component: MockComp, data: { name: 'Main Page' } },
+          { path: 'reports', pathMatch: 'prefix', component: MockComp, data: { name: 'Reports Page' } },
+          { path: 'blocks', pathMatch: 'prefix', component: MockComp, data: { name: 'Blocks Page' } },
+          { path: 'filters', pathMatch: 'prefix', component: MockComp, data: { name: 'Filters Page' } }
+        ], data: { name: 'Admin Dashboard' }};
+
+    // check the admin pages' links don't appear
+    expect(authSpy).toHaveBeenCalled();
+    expect(siteMap.routes).not.toContain(adminPath);
+    expect(navLinks[4].parentElement.parentElement.parentElement.firstElementChild.textContent).not.toBe('Admin Dashboard');
+    for(var i = 0; i < navLinks.length; i++) {
+      expect(navLinks[i].textContent).not.toBe('Main Page');
+      expect(navLinks[i].textContent).not.toBe('Reports Page');
+      expect(navLinks[i].textContent).not.toBe('Blocks Page');
+      expect(navLinks[i].textContent).not.toBe('Filters Page');
+    }
+  });
+
+  // Check that the first and last messages routes are removed
+  it('should remove the first and last children of the messages route', () => {
+    spyOn(TestBed.inject(AuthService), 'canUser').and.returnValue(false);
+    TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(SiteMap);
+    const siteMap = fixture.componentInstance;
+    const siteMapDOM = fixture.nativeElement;
+    fixture.detectChanges();
+
+    let routeList = siteMapDOM.querySelector('#routeList');
+    let navLinks = routeList!.querySelectorAll('.routerLink');
+    // the path as it should appear
+    let messagesPath = { path: 'messages',
+        children: [
+          { path: 'inbox', pathMatch: 'prefix', component: MockComp, data: { name: 'Inbox' } },
+          { path: 'outbox', pathMatch: 'prefix', component: MockComp, data: { name: 'Outbox' } },
+          { path: 'threads', pathMatch: 'prefix', component: MockComp, data: { name: 'Threads' } }
+        ], data: { name: 'Mailbox' }};
+    // the full path
+    let messagesFullPath = { path: 'messages',
+        children: [
+          { path: '', pathMatch: 'prefix', redirectTo: 'inbox', data: { name: 'Inbox' } },
+          { path: 'inbox', pathMatch: 'prefix', component: MockComp, data: { name: 'Inbox' } },
+          { path: 'outbox', pathMatch: 'prefix', component: MockComp, data: { name: 'Outbox' } },
+          { path: 'threads', pathMatch: 'prefix', component: MockComp, data: { name: 'Threads' } },
+          { path: 'thread/:id', pathMatch: 'prefix', component: MockComp, data: { name: 'Thread' } }
+        ], data: { name: 'Mailbox' }};
+
+    // check the first and last children were removed
+    expect(siteMap.routes).not.toContain(messagesFullPath);
+    expect(siteMap.routes).toContain(messagesPath);
+    expect(navLinks[4].parentElement.parentElement.children.length).toBe(3);
+    // check inbox doesn't appear twice
+    expect(navLinks[3].textContent).not.toBe('Inbox');
+    expect(navLinks[5].textContent).not.toBe('Inbox');
+    // check thread doesn't appear at all
+    for(var i = 0; i < navLinks.length; i++) {
+      expect(navLinks[i].textContent).not.toBe('Thread');
+    }
+  });
+
+  // Check that the last route is removed from user routes
+  it('should remove the last child of the user route', () => {
+    spyOn(TestBed.inject(AuthService), 'canUser').and.returnValue(false);
+    TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(SiteMap);
+    const siteMap = fixture.componentInstance;
+    const siteMapDOM = fixture.nativeElement;
+    fixture.detectChanges();
+
+    let routeList = siteMapDOM.querySelector('#routeList');
+    let navLinks = routeList!.querySelectorAll('.routerLink');
+    // the path as it should appear
+    let userPath = { path: 'user',
+        children: [
+          { path: '', pathMatch: 'prefix', component: MockComp, data: { name: 'Your Page' } }
+        ], data: { name: 'User Page' }};
+    // the full path
+    let userFullPath = { path: 'user',
+        children: [
+          { path: '', pathMatch: 'prefix', component: MockComp, data: { name: 'Your Page' } },
+          { path: ':id', pathMatch: 'prefix', component: MockComp, data: { name: 'Other User\'s Page' } }
+        ], data: { name: 'User Page' }};
+
+    // check the final child was removed
+    expect(siteMap.routes).not.toContain(userFullPath);
+    expect(siteMap.routes).toContain(userPath);
+    for(var i = 0; i < navLinks.length; i++) {
+      expect(navLinks[i].textContent).not.toBe('Other User\'s Page');
     }
   });
 });

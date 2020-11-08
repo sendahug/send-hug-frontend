4d1acdc3e7f66c9ac1c6aebfdc370c0a3ba362cf
Shir Bar Lev <shirblc@gmail.com>
2020-11-08 20:06:07 +0000

    Added basic support page.

---
+++ src/app/components/supportPage/supportPage.component.spec.ts
@@ -0,0 +1,79 @@
+/*
+	Support Page
+	Send a Hug Component Tests
+  ---------------------------------------------------
+  MIT License
+
+  Copyright (c) 2020 Send A Hug
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  The provided Software is separate from the idea behind its website. The Send A Hug
+  website and its underlying design and ideas are owned by Send A Hug group and
+  may not be sold, sub-licensed or distributed in any way. The Software itself may
+  be adapted for any purpose and used freely under the given conditions.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+import { TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from '@angular/router/testing';
+import {} from 'jasmine';
+import { APP_BASE_HREF } from '@angular/common';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from "@angular/platform-browser-dynamic/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { ServiceWorkerModule } from "@angular/service-worker";
+
+// App imports
+import { SupportPage } from "./supportPage.component";
+
+describe('Support Page', () => {
+  // Before each test, configure testing environment
+  beforeEach(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+        platformBrowserDynamicTesting());
+
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        ServiceWorkerModule.register('sw.js', { enabled: false })
+      ],
+      declarations: [
+        SupportPage
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' }
+      ]
+    }).compileComponents();
+  });
+
+  // Check the component is create
+  it('should create the component', () => {
+    const fixture = TestBed.createComponent(SupportPage);
+    const supportPage = fixture.componentInstance;
+    const supportPageDOM = fixture.nativeElement;
+    fixture.detectChanges();
+
+    expect(supportPage).toBeTruthy();
+    expect(supportPageDOM).toBeTruthy();
+  });
+});

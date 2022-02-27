2f865a4e8fec9cda8d463bdf96a4e63e4ac276f9
Shir Bar Lev <shirblc@icloud.com>
2022-02-27 12:45:14 +0000

    Updated main e2e specs for Cypress.

--- e2e/support/commands.ts
+++ e2e/support/commands.ts
@@ -1,25 +1,14 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+declare global {
+  namespace Cypress {
+      interface Chainable {
+        navigateTo(navLinkNumber: number): Chainable;
+      }
+  }
+}
+
+// navigate via clicking the nav menu link
+Cypress.Commands.add('navigateTo', (navLinkNumber: number) =>
+  cy.get('.navLink').eq(navLinkNumber).click(),
+);
+
+export {};

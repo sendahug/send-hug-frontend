a7dc6816198ca7e56588448baabaa4512f9ad708
Shir Bar Lev <shirblc@gmail.com>
2023-02-05 14:11:22 +0000

    Testing cy.origin instead of insecure chrome

--- cypress.config.ts
+++ cypress.config.ts
@@ -31,7 +31,6 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   video: false,
-  chromeWebSecurity: false,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.

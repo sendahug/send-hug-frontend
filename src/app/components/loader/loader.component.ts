d7ae5f50a8980772f676b7b1ef6657b2de7c58d1
Shir Bar Lev <shirblc@gmail.com>
2023-09-27 22:20:33 +0300

    Moved loading subscriptions from Loader to parent components.
    There's no need to check whether the data is fetched both in the loader
    and in the parent components; it's more efficient to have it only in the
    parent. It also ensures we don't add
    unneeded dependencies and
    complications to the loader component.

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -32,13 +32,6 @@
 
 // Angular imports
 import { Component, OnInit, Input, OnChanges } from "@angular/core";
-import { Subscription } from "rxjs";
-
-// App-related imports
-import { ItemsService } from "../../services/items.service";
-import { AuthService } from "../../services/auth.service";
-import { AdminService } from "../../services/admin.service";
-import { PostsService } from "../../services/posts.service";
 
 @Component({
   selector: "app-loader",
@@ -46,25 +39,12 @@ import { PostsService } from "../../services/posts.service";
 })
 export class Loader implements OnInit, OnChanges {
   // the sort of data the parent component is waiting for
-  @Input()
-  waitingFor!: string;
+  @Input() waitingFor!: string;
   // message to display to the user while waiting
   message = "";
-  // loader screen visibility
-  visible = true;
-  //
-  @Input()
-  user!: "self" | "other";
-  subsciption: Subscription | undefined;
-  subscribeCalls: number = 0;
 
   // CTOR
-  constructor(
-    private itemsService: ItemsService,
-    private authService: AuthService,
-    private adminService: AdminService,
-    private postsService: PostsService,
-  ) {}
+  constructor() {}
 
   /*
   Function Name: ngOnInit()
@@ -92,7 +72,6 @@ export class Loader implements OnInit, OnChanges {
   */
   ngOnChanges() {
     if (this.waitingFor) {
-      this.subscribeCalls = 0;
       this.checkLoadingTarget();
     }
   }
@@ -107,137 +86,25 @@ export class Loader implements OnInit, OnChanges {
   Programmer: Shir Bar Lev.
   */
   checkLoadingTarget() {
-    this.visible = true;
     // if the app is waiting for user data to be fetched from the server
-    if (this.waitingFor == "user") {
+    if (this.waitingFor == "user" || this.waitingFor == "other user") {
       this.message = "Fetching user data...";
-      // if the user's data is already fetched, skip the loading screen
-      if (this.authService.isUserDataResolved.value) {
-        this.visible = false;
-      }
-      // if not, show the loading screen until the data is fetched
-      else {
-        // subscribe to the subject following user data
-        this.subsciption = this.authService.isUserDataResolved.subscribe((value) => {
-          // the subject's value is changed to 'true' upon fetching user data,
-          // so if the value is true, there's no longer need for the loader
-          // screen.
-          if (value == true) {
-            this.visible = false;
-            this.waitingFor = "";
-            if (this.subsciption) {
-              this.subsciption.unsubscribe();
-            }
-          }
-        });
-      }
     }
-    // if the app is waiting for another user's data to be fetched from the server
-    else if (this.waitingFor == "other user") {
-      this.message = "Fetching user data...";
-      if (this.subscribeCalls == 0) {
-        this.subscribeCalls++;
-        // subscribe to the subject following other user's data
-        this.itemsService.idbResolved.user.subscribe((value) => {
-          // the subject's value is changed to 'true' upon fetching user data,
-          // so if the value is true, there's no longer need for the loader
-          // screen.
-          if (value == true) {
-            this.visible = false;
-            this.waitingFor = "";
-          }
-        });
-      }
-    }
-    // if the app is waiting for incoming messages data to be fetched from the server
-    else if (this.waitingFor == "inbox messages") {
+    // if the app is waiting for incoming/outgoing messages data to be
+    // fetched from the server
+    else if (
+      this.waitingFor == "inbox messages" ||
+      this.waitingFor == "outbox messages" ||
+      this.waitingFor == "thread messages"
+    ) {
       this.message = "Fetching messages...";
-      // subscribe to the subject following user data
-      this.authService.isUserDataResolved.subscribe((value) => {
-        // if user data has been fetched, the app can attempt to fetch
-        // the logged in user's messages
-        if (value == true && this.subscribeCalls == 0) {
-          this.subscribeCalls++;
-          // subscribe to the subject following user messages data
-          this.itemsService.idbResolved.inbox.subscribe((value) => {
-            // the subject's value is changed to 'true' upon fetching user
-            // messages, so if the value is true, there's no longer need
-            // for the loader screen
-            if (value == true) {
-              this.visible = false;
-              this.waitingFor = "";
-            }
-          });
-        }
-      });
-    }
-    // if the app is waiting for outgoing messages data to be fetched from the server
-    else if (this.waitingFor == "outbox messages") {
-      this.message = "Fetching messages...";
-      // subscribe to the subject following user data
-      this.authService.isUserDataResolved.subscribe((value) => {
-        // if user data has been fetched, the app can attempt to fetch
-        // the logged in user's messages
-        if (value == true && this.subscribeCalls == 0) {
-          this.subscribeCalls++;
-          // subscribe to the subject following user messages data
-          this.itemsService.idbResolved.outbox.subscribe((value) => {
-            // the subject's value is changed to 'true' upon fetching user
-            // messages, so if the value is true, there's no longer need
-            // for the loader screen
-            if (value == true) {
-              this.visible = false;
-              this.waitingFor = "";
-            }
-          });
-        }
-      });
     }
     // if the app is waiting for threads messages data to be fetched from the server
     else if (this.waitingFor == "threads messages") {
       this.message = "Fetching threads...";
-      // subscribe to the subject following user data
-      this.authService.isUserDataResolved.subscribe((value) => {
-        // if user data has been fetched, the app can attempt to fetch
-        // the logged in user's threads
-        if (value == true && this.subscribeCalls == 0) {
-          this.subscribeCalls++;
-          // subscribe to the subject following user threads data
-          this.itemsService.idbResolved.threads.subscribe((value) => {
-            // the subject's value is changed to 'true' upon fetching user
-            // threads, so if the value is true, there's no longer need
-            // for the loader screen
-            if (value == true) {
-              this.visible = false;
-              this.waitingFor = "";
-            }
-          });
-        }
-      });
     }
-    //
-    else if (this.waitingFor == "thread messages") {
-      this.message = "Fetching messages...";
-      // subscribe to the subject following user data
-      this.authService.isUserDataResolved.subscribe((value) => {
-        // if user data has been fetched, the app can attempt to fetch
-        // the logged in user's threads
-        if (value == true && this.subscribeCalls == 0) {
-          this.subscribeCalls++;
-          // subscribe to the subject following user threads data
-          this.itemsService.idbResolved.thread.subscribe((value) => {
-            // the subject's value is changed to 'true' upon fetching user
-            // threads, so if the value is true, there's no longer need
-            // for the loader screen
-            if (value == true) {
-              this.visible = false;
-              this.waitingFor = "";
-            }
-          });
-        }
-      });
-    }
-    // if the app is waiting for main page/new posts/suggested posts data to be fetched from the server
+    // if the app is waiting for main page/new posts/suggested
+    // posts data to be fetched from the server
     else if (
       this.waitingFor == "main page" ||
       this.waitingFor == "new posts" ||
@@ -248,87 +115,22 @@ export class Loader implements OnInit, OnChanges {
     // if the app is waiting for the user's posts to be fetched from the server
     else if (this.waitingFor == "user posts") {
       this.message = "Fetching user posts...";
-      if (this.subscribeCalls == 0) {
-        this.subscribeCalls++;
-        // subscribe to the subject following user's posts
-        this.itemsService.idbResolved.userPosts.subscribe((value) => {
-          // the subject's value is changed to 'true' upon fetching user
-          // posts, so if the value is true, there's no longer need for the
-          // loader screen
-          if (value == true) {
-            this.visible = false;
-            this.waitingFor = "";
-          }
-        });
-      }
     }
     // if the app is waiting for search results to be fetched from the server
     else if (this.waitingFor == "search") {
       this.message = "Searching...";
-      if (this.subscribeCalls == 0) {
-        this.subscribeCalls++;
-        // subscribe to the subject following search results
-        this.itemsService.isSearchResolved.subscribe((value) => {
-          // the subject's value is changed to 'true' upon fetching search
-          // results, so if the value is true, there's no longer need for the
-          // loader screen
-          if (value == true) {
-            this.visible = false;
-            this.waitingFor = "";
-          }
-        });
-      }
     }
     // if the app is waiting for open reports to be fetched from the server
     else if (this.waitingFor == "admin reports") {
       this.message = "Getting user and post reports...";
-      if (this.subscribeCalls == 0) {
-        this.subscribeCalls++;
-        // subscribe to the subject following fetching reports
-        this.adminService.isReportsResolved.subscribe((value) => {
-          // the subject's value is changed to 'true' upon fetching open
-          // reports, so if the value is true, there's no longer need for the
-          // loader screen
-          if (value == true) {
-            this.visible = false;
-            this.waitingFor = "";
-          }
-        });
-      }
     }
     // if the app is waiting for blocked users to be fetched from the server
     else if (this.waitingFor == "admin blocks") {
       this.message = "Getting blocked users...";
-      if (this.subscribeCalls == 0) {
-        this.subscribeCalls++;
-        // subscribe to the subject following fetching blocked users
-        this.adminService.isBlocksResolved.subscribe((value) => {
-          // the subject's value is changed to 'true' upon fetching blocked
-          // users, so if the value is true, there's no longer need for the
-          // loader screen
-          if (value == true) {
-            this.visible = false;
-            this.waitingFor = "";
-          }
-        });
-      }
     }
     // if the app is waiting for filtered phrases to be fetched from the server
     else if (this.waitingFor == "admin filters") {
       this.message = "Getting filtered phrases...";
-      if (this.subscribeCalls == 0) {
-        this.subscribeCalls++;
-        // subscribe to the subject following fetching filtered phrases
-        this.adminService.isFiltersResolved.subscribe((value) => {
-          // the subject's value is changed to 'true' upon fetching filtered
-          // phrases, so if the value is true, there's no longer need for the
-          // loader screen
-          if (value == true) {
-            this.visible = false;
-            this.waitingFor = "";
-          }
-        });
-      }
     }
   }
 }

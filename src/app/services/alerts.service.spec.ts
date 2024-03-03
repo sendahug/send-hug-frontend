d65914d578137f2398e802a5487efcbd216e3511
Shir Bar Lev <shirblc@gmail.com>
2024-03-03 14:27:14 +0000

    Updated the AlertsService's tests.

--- src/app/services/alerts.service.spec.ts
+++ src/app/services/alerts.service.spec.ts
@@ -36,26 +36,11 @@ import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from "@angular/platform-browser-dynamic/testing";
-import { Component } from "@angular/core";
 import { APP_BASE_HREF } from "@angular/common";
 import { HttpEventType, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
 import {} from "jasmine";
 
 import { AlertsService } from "./alerts.service";
-
-@Component({
-  selector: "app-root",
-  template: `
-    <div id="noInternet" *ngIf="alertsService.isOffline.value" role="banner">
-      You are not currently connected to the internet. For updated data, reconnect to the internet
-      and try again.
-    </div>
-    <div id="alertContainer"></div>
-  `,
-})
-class Sample {
-  constructor(private alertsService: AlertsService) {}
-}
 
 describe("AlertsService", () => {
   let alertsService: AlertsService;
@@ -67,7 +52,7 @@ describe("AlertsService", () => {
 
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [Sample],
+      declarations: [],
       providers: [AlertsService, { provide: APP_BASE_HREF, useValue: "/" }],
     }).compileComponents();
 
@@ -81,93 +66,47 @@ describe("AlertsService", () => {
 
   // Check that the service creates an alert
   it("createAlert() - should create an alert", () => {
-    const fixture = TestBed.createComponent(Sample);
-    const componentDOM = fixture.nativeElement;
-    const buildSpy = spyOn(alertsService, "buildAlertElement").and.callFake((alert) => {
-      const div = document.createElement("div");
-      div.className = `alertMessage ${alert.type}`;
+    expect(alertsService.alertMessage()).toBe("");
+    expect(alertsService.shouldDisplayAlert()).toBe(false);
+    expect(alertsService.alertType()).toBe("Success");
 
-      let closeButton = document.createElement("button");
-      closeButton.className = "appButton";
-      closeButton.id = "alertButton";
-      closeButton.textContent = "Close";
-      div.append(closeButton);
-
-      return div;
-    });
     alertsService.createAlert({
       type: "Error",
       message: "error",
     });
 
-    fixture.detectChanges();
-
-    expect(buildSpy).toHaveBeenCalled();
-    expect(componentDOM.querySelector("#alertContainer").firstElementChild).toBeTruthy();
+    expect(alertsService.alertMessage()).toBe("error");
+    expect(alertsService.shouldDisplayAlert()).toBe(true);
+    expect(alertsService.alertType()).toBe("Error");
   });
 
-  // Check the service closes the alert
-  it("closeAlert() - should close the alert", () => {
-    const fixture = TestBed.createComponent(Sample);
-    const componentDOM = fixture.nativeElement;
+  it("createAlert() - should create an alert with reload", () => {
+    expect(alertsService.shouldDisplayReloadBtn()).toBe(false);
 
-    alertsService.createAlert({
-      message: "message",
-      type: "Notification",
-    });
+    alertsService.createAlert(
+      {
+        type: "Error",
+        message: "error",
+      },
+      true,
+    );
 
-    fixture.detectChanges();
-
-    // check the alert was created in order to remove it
-    expect(componentDOM.querySelectorAll(".alertMessage")[0]).toBeTruthy();
-
-    alertsService.closeAlert();
-    fixture.detectChanges();
-
-    // check the alert was closed
-    expect(componentDOM.querySelectorAll(".alertMessage")[0]).toBeUndefined();
+    expect(alertsService.shouldDisplayReloadBtn()).toBe(true);
   });
 
-  // Check the service builds the alert element correctly
-  it("buildAlertElement() - should build the alert element", () => {
-    const alert = alertsService.buildAlertElement({
-      type: "Success",
-      message: "message",
-    });
+  it("createAlert() - should create an alert with navigation", () => {
+    expect(alertsService.shouldDisplayNavBtn()).toBe(false);
 
-    expect(alert.tagName.toLowerCase()).toBe("div");
-    expect(alert.querySelector("h3")!.textContent).toBe("Success");
-    expect(alert.querySelector("div")!.textContent).toBe("message");
-    expect(alert.querySelector("button")!.id).toBe("alertButton");
-  });
+    alertsService.createAlert(
+      {
+        type: "Error",
+        message: "error",
+      },
+      false,
+      "string",
+    );
 
-  // Check the service adds a reload button
-  it("addReloadButton() - should add a reload button", () => {
-    let alert = alertsService.buildAlertElement({
-      type: "Success",
-      message: "message",
-    });
-
-    alertsService.addReloadButton(alert);
-
-    expect(alert.querySelectorAll("button").length).toBe(2);
-    expect(alert.querySelector("#reloadBtn")).toBeTruthy();
-    expect(alert.querySelector("#reloadBtn")!.textContent).toBe("Reload Page");
-  });
-
-  // Check the service adds a navigation button
-  it("addNavigateButton() - should add navigation button", () => {
-    let alert = alertsService.buildAlertElement({
-      type: "Success",
-      message: "message",
-    });
-
-    alertsService.addNavigateButton(alert, "/");
-
-    expect(alert.querySelector("#navButton")).toBeTruthy();
-    expect(alert.querySelector("#navButton")!.tagName.toLowerCase()).toBe("a");
-    expect(alert.querySelector("#navButton")!.getAttribute("href")).toBe("/");
-    expect(alert.querySelector("#navButton")!.textContent).toBe("Home Page");
+    expect(alertsService.shouldDisplayNavBtn()).toBe(true);
   });
 
   // Check the service creates a success alert
@@ -215,32 +154,24 @@ describe("AlertsService", () => {
 
   // Check the service toggles the offline alert
   it("toggleOfflineAlert() - should add offline alert", () => {
-    const fixture = TestBed.createComponent(Sample);
-    const componentDOM = fixture.nativeElement;
     const onlineSpy = spyOnProperty(Navigator.prototype, "onLine").and.returnValue(true);
     alertsService.toggleOfflineAlert();
-    fixture.detectChanges();
 
     // expect the isOffline subject's current value to be false
     expect(alertsService.isOffline.value).toBe(false);
-    expect(componentDOM.querySelector("#noInternet")).toBeNull();
 
     // change the 'Navigator.onLine' value to false
     onlineSpy.and.returnValue(false);
     alertsService.toggleOfflineAlert();
-    fixture.detectChanges();
 
     // check the alert is on
     expect(alertsService.isOffline.value).toBe(true);
-    expect(componentDOM.querySelector("#noInternet")).toBeTruthy();
 
     // change the 'Navigator.onLine' value to true
     onlineSpy.and.returnValue(true);
     alertsService.toggleOfflineAlert();
-    fixture.detectChanges();
 
     // check the alert is gone
     expect(alertsService.isOffline.value).toBe(false);
-    expect(componentDOM.querySelector("#noInternet")).toBeNull();
   });
 });

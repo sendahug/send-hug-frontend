ab1d31eaaf89cd0be74cec7923b016368c7f4acb
Shir Bar Lev <shirblc@gmail.com>
2024-04-17 13:57:13 +0100

    Added rollup configuration file and plugins file.

---
+++ rollup.config.mjs
@@ -0,0 +1,38 @@
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import { updateComponentTemplateUrl, updateEnvironmentVariables } from "./plugins.mjs";
+
+export default (commandLineArgs) => {
+  const currentMode = commandLineArgs.mode || "live";
+
+  let buildConfig = {
+    input: ["src/main.ts"],
+    output: {
+      sourcemap: currentMode == "development" ? "inline" : "hidden",
+      dir: currentMode == "development" ? "localdev" : "dist",
+      entryFileNames: currentMode == "development" ? "app.bundle.js" : "app.bundle.min.js",
+    },
+    plugins: [
+      updateEnvironmentVariables(currentMode),
+      updateComponentTemplateUrl(),
+      typescript({ exclude: ["**/*.spec.ts", "e2e/**/*"] }),
+      nodeResolve({
+        extensions: [".js", ".ts"],
+      }),
+      commonjs({
+        extensions: [".js", ".ts"],
+        transformMixedEsModules: true,
+      }),
+    ],
+  };
+
+  // Only run terser in production, as it's quite a long and expensive
+  // process.
+  if (currentMode != "development") {
+    buildConfig.output.plugins = [terser()];
+  }
+
+  return buildConfig;
+};

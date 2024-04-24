1366e602bc3bd30892f0c6fd22c4a26862635598
Shir Bar Lev <shirblc@gmail.com>
2024-04-24 14:11:01 +0300

    Updated the structure of the e2e setup script to the new backend setup.

--- e2e/setup.py
+++ e2e/setup.py
@@ -1,16 +1,19 @@
 from create_app import create_app
-from models.models import db
+from config import SAHConfig
 from tests.data_models import create_data
+from models.models import BaseModel
 
 
 def setup_e2e():
     """Sets up the database ahead of e2e tests"""
     test_db_path = "postgresql://postgres:password@localhost:5432/test_sah"
-    app = create_app(test_db_path)
+    test_config = SAHConfig(database_url=test_db_path)
+    app = create_app(config=test_config)
     with app.app_context():
-        db.drop_all()
-        db.create_all()
-        create_data(db)
+        BaseModel.metadata.drop_all(test_config.db.engine)
+        # create all tables
+        BaseModel.metadata.create_all(test_config.db.engine)
+        create_data(test_config.db)
 
 if __name__ == "__main__":
     setup_e2e()

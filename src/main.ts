40da01d8564cee8422d43ff5c53568fd34ae2b69
Shir Bar Lev <shirblc@gmail.com>
2024-08-03 20:41:19 +0100

    Fixed two more copyright errors.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 /*
   Main entrypoint
-  Send a Hug Component
   ---------------------------------------------------
   MIT License
 
-  Copyright (c) 2024 Shir Bar Lev
+  Copyright (c) 2020-2024 Send A Hug
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal

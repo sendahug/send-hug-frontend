96ca00b4b3d1e040f1495fe6b18255a50d860569
Shir Bar Lev <shirblc@gmail.com>
2024-08-03 20:19:26 +0100

    Fixed the copyright notices.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@
   ---------------------------------------------------
   MIT License
 
-  Copyright (c) 2024 Shir Bar Lev
+  Copyright (c) 2020-2024 Send A Hug
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal

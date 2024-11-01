0c396de62cac327e5184fd97f7f134cea489d8dd
Shir Bar Lev <shirblc@gmail.com>
2024-11-01 20:12:46 +0000

    Testing the updated structure for e2e tests' setup.

--- e2e/setup.py
+++ e2e/setup.py
@@ -1,34 +1,44 @@
 import asyncio
 
 from firebase_admin import initialize_app  # type: ignore
+from sqlalchemy import URL
 
 from create_app import create_app
 from tests.data_models import create_data
 from models.common import BaseModel
 from models.db import SendADatabase
+from config.config import sah_config
 
-class SAHConfig:
-    """
-    Configuration class for the Send A Hug backend.
-    """
+# class SAHConfig:
+#     """
+#     Configuration class for the Send A Hug backend.
+#     """
 
-    def __init__(self, database_url: str):
-        self.database_url = database_url
-        self.db = SendADatabase(database_url=database_url)
-        self.firebase_app = initialize_app()
+#     def __init__(self, database_url: str):
+#         self.database_url = database_url
+#         self.db = SendADatabase(database_url=URL.create(
+#                 drivername="postgresql+asyncpg",
+#                 username="postgres",
+#                 password="password",
+#                 host="localhost",
+#                 port=5432,
+#                 database="sendahug",
+#             )
+#         )
+#         self.firebase_app = initialize_app()
 
 
 async def setup_e2e():
     """Sets up the database ahead of e2e tests"""
-    test_db_path = "postgresql+asyncpg://postgres:password@localhost:5432/sendahug"
-    test_config = SAHConfig(database_url=test_db_path)
-    app = create_app(config=test_config)
+    # test_db_path = "postgresql+asyncpg://postgres:password@localhost:5432/sendahug"
+    # test_config = SAHConfig(database_url=test_db_path)
+    app = create_app()
 
-    async with test_config.db.engine.begin() as conn:
+    async with sah_config.db.engine.begin() as conn:
         await conn.run_sync(BaseModel.metadata.drop_all)
         await conn.run_sync(BaseModel.metadata.create_all)
 
-    await create_data(test_config.db)
+    await create_data(sah_config.db)
 
 
 if __name__ == "__main__":

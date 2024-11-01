63cc1581fce8c736b86cc65313fc6b82dac42a2b
Shir Bar Lev <shirblc@gmail.com>
2024-11-01 21:00:56 +0000

    Deleted the commented-out code from setup.py.

--- e2e/setup.py
+++ e2e/setup.py
@@ -1,31 +1,9 @@
 import asyncio
 
-from firebase_admin import initialize_app  # type: ignore
-from sqlalchemy import URL
-
 from create_app import create_app
 from tests.data_models import create_data
 from models.common import BaseModel
-from models.db import SendADatabase
 from config.config import sah_config
-
-# class SAHConfig:
-#     """
-#     Configuration class for the Send A Hug backend.
-#     """
-
-#     def __init__(self, database_url: str):
-#         self.database_url = database_url
-#         self.db = SendADatabase(database_url=URL.create(
-#                 drivername="postgresql+asyncpg",
-#                 username="postgres",
-#                 password="password",
-#                 host="localhost",
-#                 port=5432,
-#                 database="sendahug",
-#             )
-#         )
-#         self.firebase_app = initialize_app()
 
 
 async def setup_e2e():

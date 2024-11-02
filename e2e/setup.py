1ce3417885078efae074b852e0d863ffeccead25
Shir Bar Lev <51282497+shirblc@users.noreply.github.com>
2024-11-02 10:05:00 +0000

    chore: Fixed e2e tests in CI. (#1878)
    * Testing the updated structure for e2e tests' setup.
    
    * Moved the location of the test credentials.
    
    * Added a print for the current working directory.
    
    * Changed the firebase credentials file's path to absolute.
    
    * Changed the echo command's location...
    
    * Added a cd.
    
    * More debugging tests.
    
    * Hopefully the right structure now
    
    * Forgot to fix the env var's path.
    
    * Fixed the a11y tests setup too.
    
    * Removed the extra print.
    
    * Deleted the commented-out code from setup.py.
    
    * Changed browser install in e2e and a11y tests to only install Chrome.
    
    * Added changelog entry.

--- e2e/setup.py
+++ e2e/setup.py
@@ -1,34 +1,22 @@
 import asyncio
 
-from firebase_admin import initialize_app  # type: ignore
-
 from create_app import create_app
 from tests.data_models import create_data
 from models.common import BaseModel
-from models.db import SendADatabase
-
-class SAHConfig:
-    """
-    Configuration class for the Send A Hug backend.
-    """
-
-    def __init__(self, database_url: str):
-        self.database_url = database_url
-        self.db = SendADatabase(database_url=database_url)
-        self.firebase_app = initialize_app()
+from config.config import sah_config
 
 
 async def setup_e2e():
     """Sets up the database ahead of e2e tests"""
-    test_db_path = "postgresql+asyncpg://postgres:password@localhost:5432/sendahug"
-    test_config = SAHConfig(database_url=test_db_path)
-    app = create_app(config=test_config)
+    # test_db_path = "postgresql+asyncpg://postgres:password@localhost:5432/sendahug"
+    # test_config = SAHConfig(database_url=test_db_path)
+    app = create_app()
 
-    async with test_config.db.engine.begin() as conn:
+    async with sah_config.db.engine.begin() as conn:
         await conn.run_sync(BaseModel.metadata.drop_all)
         await conn.run_sync(BaseModel.metadata.create_all)
 
-    await create_data(test_config.db)
+    await create_data(sah_config.db)
 
 
 if __name__ == "__main__":

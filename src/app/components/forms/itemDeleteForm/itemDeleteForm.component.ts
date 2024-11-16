4f1d973a1df552da01ae25ff808b241e4a417fdd
Shir Bar Lev <shirblc@gmail.com>
2024-11-16 18:14:09 +0000

    Added specific typing for ItemDeleteForm's messType.

--- src/app/components/forms/itemDeleteForm/itemDeleteForm.component.ts
+++ src/app/components/forms/itemDeleteForm/itemDeleteForm.component.ts
@@ -42,6 +42,7 @@ import { SWManager } from "@app/services/sWManager.service";
 import { AlertsService } from "@app/services/alerts.service";
 import { PopUp } from "@common/popUp/popUp.component";
 import { TeleportDirective } from "@app/directives/teleport.directive";
+import { type MessageType } from "@app/interfaces/types";
 
 @Component({
   selector: "item-delete-form",
@@ -57,7 +58,7 @@ export class ItemDeleteForm {
   @Input() toDelete: string | undefined;
   // the item to delete itself
   @Input() itemToDelete: number | undefined;
-  @Input() messType: string | undefined;
+  @Input() messType: MessageType | undefined;
   @Input() reportData: any;
 
   // CTOR

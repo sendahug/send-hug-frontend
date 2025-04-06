00a76a6830f3f5adf011fe8a550ebb78d108738d
Shir Bar Lev <shirblc@gmail.com>
2025-04-06 13:56:28 +0100

    Bumped the vendor size limit

--- .size-limit.ts
+++ .size-limit.ts
@@ -78,7 +78,7 @@ module.exports = [
   },
   {
     name: "Vendor scripts",
-    limit: "230 kB",
+    limit: "232 kB",
     path: ["dist/assets/vendor-*.js", "dist/assets/polyfills-*.js"],
   },
   {

12b27af86667ee3db1ae6c47d1978ff7ffe41122
Shir Bar Lev <shirblc@gmail.com>
2025-07-14 18:37:55 +0100

    Removed zone from the tests setup

--- plugins/tests.ts
+++ plugins/tests.ts
@@ -22,17 +22,22 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-import "zone.js";
-import "zone.js/testing";
 import { getTestBed } from "@angular/core/testing";
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
-} from "@angular/platform-browser-dynamic/testing";
+import { BrowserTestingModule, platformBrowserTesting } from "@angular/platform-browser/testing";
 import "@angular/compiler";
+import { NgModule, provideZonelessChangeDetection } from "@angular/core";
+
+@NgModule({
+  providers: [provideZonelessChangeDetection()],
+})
+class ZonelessTestingModule {}
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
-  errorOnUnknownElements: true,
-  errorOnUnknownProperties: true,
-});
+getTestBed().initTestEnvironment(
+  [BrowserTestingModule, ZonelessTestingModule],
+  platformBrowserTesting(),
+  {
+    errorOnUnknownElements: true,
+    errorOnUnknownProperties: true,
+  },
+);
